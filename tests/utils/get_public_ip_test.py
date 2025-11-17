99593c9110d5300ee6a1d22aaf6fa693b74fbd97
dimaslanjaka <dimaslanjaka@gmail.com>
2025-11-17 20:32:30 +0700

    test(get_public_ip): add cache and verbose options to get_public_ip test

--- tests/utils/get_public_ip_test.py
+++ tests/utils/get_public_ip_test.py
@@ -1,5 +1,7 @@
 from proxy_checker.utils.get_public_ip import get_public_ip
 
-res = get_public_ip(proxy_info={"proxy": "16.78.93.162:25731"})
+res = get_public_ip(
+    proxy_info={"proxy": "16.78.93.162:25731"}, cache=True, verbose=True
+)
 if res:
     print(f"My public IP is: {res}")
