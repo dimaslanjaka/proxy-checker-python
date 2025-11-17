676e8760dc38e34f530ba1ea12088af9c24207da
dimaslanjaka <dimaslanjaka@gmail.com>
2025-11-17 13:21:38 +0700

    test(proxy-checker): update proxy address and enhance check_proxy call with additional parameters

--- tests/test.py
+++ tests/test.py
@@ -2,9 +2,13 @@ from proxy_checker import ProxyChecker
 
 checker = ProxyChecker(verbose=False)
 print(f"my IP {checker.ip}")
-proxy = "3.98.231.179:3128"
+proxy = "157.175.43.137:797"
 ip = proxy.split(":")[0]
 print(f"proxy IP {ip}")
-result = checker.check_proxy(proxy, True)
+result = checker.check_proxy(
+    proxy=proxy, check_all_protocols=True, check_address=True, check_country=True
+)
 print(result)
+print(f"latency: {result.latency} ms")
+print(f"anonymity: {result.anonymity}")
 print(f"is proxy ip same {checker.ip == ip}")
